80df754b185ca05ab5c06949efce3b6b08eb36ee
Joseph Wynn <joseph.wynn@bbc.co.uk>
2015-03-17 22:58:22 +0000

    Create separate Integer/Float tests for multiplication and division

--- test/primitives.sh
+++ test/primitives.sh
@@ -15,12 +15,22 @@ it_performs_subtraction_below_zero() {
     test "$result" = "-11"
 }
 
-it_performs_multiplication() {
+it_performs_multiplication_with_integers() {
     result=$(bin/lishp "(* 2 3)")
     test "$result" = "6"
 }
 
-it_performs_division() {
-    result=$(bin/lishp "(/ 1 2)")
+it_performs_multiplication_with_floats() {
+    result=$(bin/lishp "(* 3 1.5)")
+    test "$result" = "4.5"
+}
+
+it_performs_division_with_integers() {
+    result=$(bin/lishp "(/ 6 2)")
+    test "$result" = "3"
+}
+
+it_performs_division_with_floats() {
+    result=$(bin/lishp "(/ 1.0 2)")
     test "$result" = "0.5"
 }
