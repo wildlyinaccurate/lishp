34b11c6db5de4bd17ca5ac2f327a8436bb85eb4b
Joseph Wynn <joseph@wildlyinaccurate.com>
2015-03-23 22:57:51 +0000

    Tidy up
     - Move code concerned with types into Lishp.Types
     - Remove unused Lishp.Interpreter exports
     - Add explicit type declaration to trapError
     - Fix indentation

--- src/Lishp/Interpreter.hs
+++ src/Lishp/Interpreter.hs
@@ -1,11 +1,4 @@
-module Lishp.Interpreter
-    (
-      readExpr
-    , eval
-    , evalString
-    , extractValue
-    , trapError
-    ) where
+module Lishp.Interpreter (evalString) where
 
 import Control.Monad
 import Control.Monad.Error
@@ -81,6 +74,12 @@ readExpr input = case parse parseExpr "lisp" input of
 evalString :: String -> IO String
 evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
 
+trapError :: ThrowsError String -> ThrowsError String
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
+
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
 eval val@(Integer _) = return val
@@ -94,31 +93,3 @@ apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
                         ($ args)
                         (lookup func primitives)
-
-instance Show LispVal where
-    show (String s) = show s
-    show (Integer n) = show n
-    show (Float n) = show n
-    show (Bool b) = if b then "#t" else "#f"
-    show (List l) = "(" ++ unwordsList l ++ ")"
-
-showError :: LispError -> String
-showError (UnboundVar message varname)  = message ++ ": " ++ varname
-showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func)    = message ++ ": " ++ show func
-showError (NumArgs expected found)      = "Expected " ++ show expected
-                                       ++ " args; found values " ++ unwordsList found
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
-                                       ++ ", found " ++ show found
-showError (Parser parseErr)             = "Parse error at " ++ show parseErr
-showError (DivisionByZero)              = "Division by zero!"
-
-instance Show LispError where show = showError
-
-trapError action = catchError action (return . show)
-
-extractValue :: ThrowsError a -> a
-extractValue (Right val) = val
-
-unwordsList :: [LispVal] -> String
-unwordsList = unwords . map show
--- src/Lishp/Types.hs
+++ src/Lishp/Types.hs
@@ -11,16 +11,23 @@ data LispVal = Atom String
              | String String
              | Bool Bool
 
+instance Show LispVal where
+    show (String s) = show s
+    show (Integer n) = show n
+    show (Float n) = show n
+    show (Bool b) = if b then "#t" else "#f"
+    show (List l) = "(" ++ unwordsList l ++ ")"
+
 instance Eq (LispVal) where
-  Atom a == Atom b = a == b
-  List a == List b = a == b
-  Integer a == Integer b = a == b
-  Float a == Float b = a == b
-  Integer a == Float b = fromIntegral a == b
-  Float a == Integer b = a == fromIntegral b
-  String a == String b = a == b
-  Bool a == Bool b = a == b
-  _ == _ = False
+    Atom a == Atom b = a == b
+    List a == List b = a == b
+    Integer a == Integer b = a == b
+    Float a == Float b = a == b
+    Integer a == Float b = fromIntegral a == b
+    Float a == Integer b = a == fromIntegral b
+    String a == String b = a == b
+    Bool a == Bool b = a == b
+    _ == _ = False
 
 type ThrowsError = Either LispError
 
@@ -34,5 +41,21 @@ data LispError = NumArgs Integer [LispVal]
                | Default String
 
 instance Error LispError where
-     noMsg = Default "An error has occurred"
-     strMsg = Default
+    noMsg = Default "An error has occurred"
+    strMsg = Default
+
+instance Show LispError where show = showError
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected
+                                       ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+showError (DivisionByZero)              = "Division by zero!"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map show
