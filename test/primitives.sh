7d1c1d85c622eb2c6b02f232598c6e7987193d0f
Joseph Wynn <joseph@wildlyinaccurate.com>
2015-03-30 22:30:17 +0100

    Refactor tests

--- test/primitives.sh
+++ test/primitives.sh
@@ -1,71 +1,33 @@
-describe "Primitives"
+describe 'Primitives'
 
 it_performs_addition() {
-    result=$($LISHP "(+ 1 3 9)")
-    test "$result" = "13"
-}
-
-it_performs_addition_with_negative_numbers() {
-    result=$($LISHP "(+ -1 3)")
-    test "$result" = "2"
-}
-
-it_performs_addition_with_floats() {
-    result=$($LISHP "(+ 1.5 3)")
-    test "$result" = "4.5"
+    test $($LISHP '(+ 1 3 9)') = '13'
+    test $($LISHP '(+ -1 3)') = '2'
+    test $($LISHP '(+ 1.5 3)') = '4.5'
 }
 
 it_performs_subtraction() {
-    result=$($LISHP "(- 9 3 1)")
-    test "$result" = "5"
+    test $($LISHP '(- 9 3 1)') = '5'
+    test $($LISHP '(- -5 2)') = '-7'
+    test $($LISHP '(- 3 1.5)') = '1.5'
+    test $($LISHP '(- 1 3 9)') = '-11'
 }
 
-it_performs_subtraction_with_negative_numbers() {
-    result=$($LISHP "(- -5 2)")
-    test "$result" = "-7"
+it_performs_multiplication() {
+    test $($LISHP '(* 2 3)') = '6'
+    test $($LISHP '(* 3 1.5)') = '4.5'
 }
 
-it_performs_subtraction_with_floats() {
-    result=$($LISHP "(- 3 1.5)")
-    test "$result" = "1.5"
-}
-
-it_performs_subtraction_below_zero() {
-    result=$($LISHP "(- 1 3 9)")
-    test "$result" = "-11"
-}
-
-it_performs_multiplication_with_integers() {
-    result=$($LISHP "(* 2 3)")
-    test "$result" = "6"
-}
-
-it_performs_multiplication_with_floats() {
-    result=$($LISHP "(* 3 1.5)")
-    test "$result" = "4.5"
-}
-
-it_performs_division_with_integers() {
-    result=$($LISHP "(/ 8 2 2)")
-    test "$result" = "2"
-}
-
-it_performs_division_with_floats() {
-    result=$($LISHP "(/ 1.0 2)")
-    test "$result" = "0.5"
-}
-
-it_throws_an_error_on_divide_by_zero() {
-    result=$($LISHP "(/ 5 0)")
-    test "$result" = "Division by zero!"
+it_performs_division() {
+    test $($LISHP '(/ 8 2 2)') = '2'
+    test $($LISHP '(/ 1.0 2)') = '0.5'
+    test "$($LISHP '(/ 5 0)')" = 'Division by zero!'
 }
 
 it_performs_modulus() {
-    result=$($LISHP "(mod 4 3)")
-    test "$result" = "1"
+    test $($LISHP '(mod 4 3)') = '1'
  }
 
 it_calculates_division_remainder() {
-    result=$($LISHP "(remainder 5 2)")
-    test "$result" = "1"
+    test $($LISHP '(remainder 5 2)') = '1'
  }
--- test/types.sh
+++ test/types.sh
@@ -1,46 +1,37 @@
 describe "Basic type parsing"
 
 it_parses_strings() {
-    result=$($LISHP '"foo bar"')
-    test "$result" = '"foo bar"'
+    test "$($LISHP '"foo bar"')" = '"foo bar"'
 }
 
 it_parses_escaped_strings() {
-    result=$($LISHP '"foo \"baz\" bar"')
-    test "$result" = '"foo \"baz\" bar"'
+    test "$($LISHP '"foo \"baz\" bar"')" = '"foo \"baz\" bar"'
 }
 
 it_parses_positive_integers() {
-    result=$($LISHP 1048)
-    test "$result" = "1048"
+    test $($LISHP 1048) = '1048'
 }
 
 it_parses_negative_integers() {
-    result=$($LISHP "-1048")
-    test "$result" = "-1048"
+    test $($LISHP '-1048') = '-1048'
 }
 
 it_parses_floats() {
-    result=$($LISHP 10.48)
-    test "$result" = "10.48"
+    test $($LISHP 10.48) = '10.48'
 }
 
 it_parses_boolean_true() {
-    result=$($LISHP '#t')
-    test "$result" = "#t"
+    test $($LISHP '#t') = '#t'
 }
 
 it_parses_boolean_false() {
-    result=$($LISHP '#f')
-    test "$result" = "#f"
+    test $($LISHP '#f') = '#f'
 }
 
 it_parses_lists() {
-    result=$($LISHP "'(1 2 3)")
-    test "$result" = "(1 2 3)"
+    test "$($LISHP "'(1 2 3)")" = "(1 2 3)"
 }
 
 it_parses_nested_lists() {
-    result=$($LISHP "'(1 2 (3))")
-    test "$result" = "(1 2 (3))"
+    test "$($LISHP "'(1 2 (3))")" = "(1 2 (3))"
 }
